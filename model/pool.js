d47b81cf2f63dad23d7ce4c9a7af0be76dc42de2
Damon Thomas <damon22t@gmail.com>
2024-11-17 23:31:24 -0500

    pool fix

--- model/pool.js
+++ model/pool.js
@@ -2,7 +2,5 @@ const { Pool } = require("pg");
 require('dotenv').config()
 
 module.exports = new Pool({
-  connectionString: 'postgres:LIjpltdFfOVPCKUFSqnnvoeXRtieTBee@autorack.proxy.rlwy.net:59205/railway'
-
-  
+  connectionString: 'postgres:LIjpltdFfOVPCKUFSqnnvoeXRtieTBee@autorack.proxy.rlwy.net:59205/railway' 
 });
